e888080460ba06de8543b30d70168b0f0674ab59
kohan123 <sportodavka@gmail.com>
2023-04-03 20:13:53 +0200

    feat: add modal logic

--- js/index.js
+++ js/index.js
@@ -95,5 +95,46 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
           }      
         }
-    setClock('.timer', deadline);    
+    setClock('.timer', deadline);
+    
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal () {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);
+    }      
+
+        modalTrigger.forEach(btn => {
+          btn.addEventListener('click', openModal);
+        });
+    
+    modal.addEventListener('click', closeModal); 
+  
+  function closeModal () {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  };
+
+  document.addEventListener('keydown', (e) => {
+    if(e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  })
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll () {
+    if (window.pageYOffset + document.documentElement.clientHeight >= 
+      document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll)
+      }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
